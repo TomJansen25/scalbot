1337f27374fab54cbc84795c155170471aab88d0
Tom Jansen <tomjansen25@gmail.com>
2022-05-17 22:55:24 +0200

    add Symbols to Enum and update MAKEFILE to run outside of poetry environment

--- scalbot/enums/symbol.py
+++ scalbot/enums/symbol.py
@@ -7,6 +7,8 @@ class Symbol(Enum):
     Enum Class to define which Symbols can be traded
     """
 
+    ADAUSD = "ADAUSD"
+    BITUSD = "BITUSD"
     BTCUSD = "BTCUSD"
     ETHUSD = "ETHUSD"
     XRPUSD = "XRPUSD"
